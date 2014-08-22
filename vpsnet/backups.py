840743819e2c9ed6747c40bd4df1043fc776b472
Vitaliy Zhhuta <vitaliyz@uk2group.com>
2014-08-22 13:52:41 +0300

    renamed list to list_all

--- vpsnet/backups.py
+++ vpsnet/backups.py
@@ -2,13 +2,15 @@ __author__ = 'zhhuta'
 
 from .utils import request_handler
 
-def list(cs_id):
+
+def list_all(cs_id):
     """
 
     :param cs_id:
     :return:
     """
     return request_handler(cs_id=cs_id, name="backup_lists")
+
 
 def rsync_info(cs_id):
     """
@@ -18,28 +20,31 @@ def rsync_info(cs_id):
     """
     return request_handler(cs_id=cs_id, name="rsync_backup_info")
 
+
 def create_template(cs_id):
     """
 
     :param cs_id:
     :return:
     """
-    return request_handler(cs_id=cs_id,name="create_template")
+    return request_handler(cs_id=cs_id, name="create_template")
 
-def convert2template(cs_id,backup_id):
+
+def convert2template(cs_id, backup_id):
     """
 
     :param cs_id:
     :param backup_id:
     :return:
     """
-    return request_handler(cs_id=(cs_id,backup_id),name="cs_id,backup_id")
+    return request_handler(cs_id=(cs_id, backup_id), name="cs_id,backup_id")
 
-def delete(cs_id,backup_id):
+
+def delete(cs_id, backup_id):
     """
 
     :param cs_id:
     :param backup_id:
     :return:
     """
-    return request_handler(cs_id=(cs_id,backup_id), name="backup_delete")
+    return request_handler(cs_id=(cs_id, backup_id), name="backup_delete")
